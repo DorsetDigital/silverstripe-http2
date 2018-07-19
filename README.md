268684d0958122523b31892d2c7029a8e3685235
Tim Burt <tim@dorset-digital.net>
2018-07-19 17:06:15 +0100

    Update headers, readme

--- README.md
+++ README.md
@@ -6,5 +6,15 @@ Adds basic HTTP2 functionality like requirements push
 *Silverstripe 4.x
 
 
+#Usage
 
+Add a config YML file to enable:
 
+```
+
+---
+Name: httptest
+---
+DorsetDigital\HTTP2\DDMiddleware:
+  enabled: true
+```
--- src/DDMiddleware.php
+++ src/DDMiddleware.php
@@ -2,15 +2,17 @@
 
 namespace DorsetDigital\HTTP2;
 
-use    SilverStripe\Control\Middleware\HTTPMiddleware;
-use    SilverStripe\Control\HTTPRequest;
-use    SilverStripe\Core\Config\Configurable;
-use    SilverStripe\Core\Injector\Injectable;
-use    SilverStripe\Control\Director;
+use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\Control\Director;
 use SilverStripe\View\Requirements;
 use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Core\Manifest\ResourceURLGenerator;
 
-class    DDMiddleware implements HTTPMiddleware
+class DDMiddleware implements HTTPMiddleware
 {
 
     use    Injectable;
@@ -42,15 +44,30 @@ class    DDMiddleware implements HTTPMiddleware
         $js = array_keys($reqs->getJavascript());
         $headerParts = [];
         foreach ($js as $script) {
-            $headerParts[] = '<'.Director::absoluteURL($script).'>; rel=preload; as=script';
+            $headerParts[] = '<' . $this->pathForFile($script) . '>; rel=preload; as=script';
         }
 
-        $css = $reqs->getCSS();
+        $css = array_keys($reqs->getCSS());
 
-        $scripts = implode(",", $newJS);
-        $response->addHeader('X-Link', $scripts);
+        foreach ($css as $style) {
+            $headerParts[] = '<' . $this->pathForFile($style) . '>; rel=preload; as=style';
+        }
+
+
+        $scripts = implode(",", $headerParts);
+        $response->addHeader('Link', $scripts);
 
         return $response;
+    }
+
+    private function pathForFile($fileOrUrl)
+    {
+        // Since combined urls could be root relative, treat them as urls here.
+        if (preg_match('{^(//)|(http[s]?:)}', $fileOrUrl) || Director::is_root_relative_url($fileOrUrl)) {
+            return $fileOrUrl;
+        } else {
+            return Injector::inst()->get(ResourceURLGenerator::class)->urlForResource($fileOrUrl);
+        }
     }
 
 
